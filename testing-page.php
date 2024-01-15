555828ea6cdd7f974fc97e7b0c47931f78064cf0
chrisn02 <59711723+chrisn02@users.noreply.github.com>
2024-01-15 14:57:43 +0000

    Added slightly more to page
    Just working on basic HTML structure so far

--- testing-page.php
+++ testing-page.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testing Page</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <!--<link rel="stylesheet" href="assets/css/style.css">-->
 </head>
 <body>
     <header>
@@ -16,5 +16,15 @@
             </ul>
         </nav>
     </header>
+    <p>Subject<p>
+    <p>Test Name<p>
+    <p>Question text box<p>
+    <p>Question 1/10<p>
+    <p>Answer 1<p>
+    <p>Answer 2<p>
+    <p>Answer 3<p>
+    <p>Answer 4<p>
+    
+    
 </body>
 </html>
