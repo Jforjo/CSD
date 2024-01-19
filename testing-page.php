20a6d825d19fa8e602bcf62912492713cec82cf2
chrisn02 <chrisnorman645@gmail.com>
2024-01-19 19:17:52 +0000

    Created the answer boxes
    Created the answer boxes that the user will click on. Very basic design so far

--- testing-page.php
+++ testing-page.php
@@ -6,6 +6,25 @@
     <title>Testing Page</title>
     <!--<link rel="stylesheet" href="assets/css/style.css">-->
 </head>
+<style>
+    .answers
+    {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 10px;
+        width: 50%;
+    }
+    .answer
+    {
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+    }
+    .answer:hover
+    {
+        background-color: #e6e6e6;
+    }
+</style>  
 <body>
     <header>
         <nav>
@@ -24,6 +43,14 @@
     <p>Answer 2<p>
     <p>Answer 3<p>
     <p>Answer 4<p>
+
+    <div class="answers">
+        <div class="answer">Answer 1</div>
+        <div class="answer">Answer 2</div>
+        <div class="answer">Answer 3</div>
+        <div class="answer">Answer 4</div>
+    </div>
+    
     
     
 </body>
