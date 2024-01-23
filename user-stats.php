0582dde8c446b2649523262b528babe547140405
chrisn02 <59711723+chrisn02@users.noreply.github.com>
2024-01-23 13:39:21 +0000

    User Stats page started

---
+++ user-stats.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Stats</title>
+    <style>
+    *
+    {
+        font-family: Arial, Helvetica, sans-serif;
+        color: white;
+        background-color: #0f0f0f;
+    }
+    
+    nav ul 
+    {
+        display: flex;
+        list-style-type: none;
+        justify-content: center;
+    }
+
+    nav ul li
+    {
+        margin: 0 20px;
+    }
+    </style>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Stats</a></li>
+                <li><a href="#">Leaderboards</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
