337906304f9b6049935f0030268ca0c77a5cf2da
Jforjo <jaholmes2002@hotmail.com>
2024-01-31 07:46:24 +0000

    Working nav links & more organised file structure The content in the <main> tag on the "admin-dashboard.html" page has been removed and put into its own file named "dashboard.html". Added the files for the management pages to by asyncronously put into the index page. Changes "admin-dashboard.html" to "index.html". Moved all the html files inside a folder named "admin" within the "pages" folder.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,47 @@
-const navList = document.querySelectorAll('nav ul li');
-navList.forEach((navItem, index) => {
-    if (navItem.classList.contains('listening')) return;
-    navItem.addEventListener('click', (e) => {
-        // Prevent the a tag from redirecting
-        e.preventDefault();
-        // If it's already active
-        if (navItem.classList.contains('active')) return;
-        // Remove active class from the previous active link
-        navList.forEach(navItem => {
-            navItem.classList.remove('active');
-            navItem.querySelector('svg')?.classList.remove('nc-int-icon-state-b');
-        });
-        // make this link active
-        navItem.querySelector('svg')?.classList.add('nc-int-icon-state-b');
-        navItem.classList.add('active');
-        document.querySelector('nav .indicator')?.style.setProperty('--pos', index);
-        // Load the page
-        linkHandler(navItem.querySelector('a')?.href);
-    });
-});
+(() => {
+    "use strict";
 
-function linkHandler(link) {
-    return;
-}
+    window.onload = (e) => {
+        LinkHandler('admin/dashboard.html');
+    };
+
+    const navList = document.querySelectorAll('nav ul li');
+    navList.forEach((navItem, index) => {
+        if (navItem.classList.contains('listening')) return;
+        navItem.addEventListener('click', (e) => {
+            // Prevent the a tag from redirecting
+            e.preventDefault();
+            // If it's already active
+            if (navItem.classList.contains('active')) return;
+            // Remove active class from the previous active link
+            navList.forEach(navItem => {
+                navItem.classList.remove('active');
+                navItem.querySelector('svg')?.classList.remove('nc-int-icon-state-b');
+            });
+            // make this link active
+            navItem.querySelector('svg')?.classList.add('nc-int-icon-state-b');
+            navItem.classList.add('active');
+            document.querySelector('nav .indicator')?.style.setProperty('--pos', index);
+            // Load the page
+            LinkHandler(navItem.querySelector('a')?.dataset?.link);
+        });
+    });
+    
+    function LinkHandler(link) {
+        if (link == null) return;
+        fetch(`/pages/${link}`, {
+            // body: formData
+        }).then(res => {
+            if (res.status >= 200 && res.status < 300) {
+                return res.text();
+            }
+            throw new Error(res.statusText);
+        }).then(data => {
+            document.body.querySelector('main').innerHTML = data;
+            Breadcrumbs(link);
+        }).catch(error => {
+            if (error === null || error === '') error = "An Unknown Error Occurred";
+            console.error("error", error);
+        });
+    }
+})();
