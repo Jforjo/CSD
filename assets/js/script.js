53aa2cf4cb13bb537ad0adc66647da6cd909aa04
Jforjo <jaholmes2002@hotmail.com>
2024-02-05 15:07:59 +0000

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@
     "use strict";
 
     window.onload = (e) => {
-        LinkHandler('admin/dashboard.html');
+        LinkHandler('admin/dashboard.html', 'Dashboard', {
+            'admin': 'admin/dashboard.html',
+            'dashboard': 'admin/dashboard.html'
+        });
     };
 
     const navList = document.querySelectorAll('nav ul li');
@@ -23,11 +26,15 @@
             navItem.classList.add('active');
             document.querySelector('nav .indicator')?.style.setProperty('--pos', index);
             // Load the page
-            LinkHandler(navItem.querySelector('a')?.dataset?.link);
+            let link = navItem.querySelector('a')?.dataset?.link;
+            // Turns "admin/student-management.html" into "Student Management"
+            let title = link?.split('/').pop().split('.')[0].split('-').map(str => str.charAt(0).toUpperCase() + str.substring(1)).join(' ');
+            LinkHandler(link, title);
+            
         });
     });
     
-    function LinkHandler(link) {
+    function LinkHandler(link, title = null, breadcrumbs = null) {
         if (link == null) return;
         fetch(`/pages/${link}`, {
             // body: formData
@@ -38,10 +45,43 @@
             throw new Error(res.statusText);
         }).then(data => {
             document.body.querySelector('main').innerHTML = data;
-            Breadcrumbs(link);
+            title != null && (document.getElementById('page-title').innerText = title);
+            // breadcrumbs != null && Breadcrumbs(breadcrumbs);
+            // history.pushState({}, "", '/' + link.split('.')[0]);
         }).catch(error => {
             if (error === null || error === '') error = "An Unknown Error Occurred";
             console.error("error", error);
         });
     }
+    /*
+    function Breadcrumbs(crumbs) {
+        if (crumbs == null) return;
+        if (typeof crumbs !== 'object') return;
+        const breadcrumbsElement = document.getElementById('breadcrumbs');
+        breadcrumbsElement.innerHTML = '';
+        Object.keys(crumbs).forEach((key, index, arr) => {
+            // The "data-link" existing decides whether it is underlined or not by the CSS.
+            if (crumbs[key] == '')
+                breadcrumbsElement.innerHTML += `<a>${key}</a>`;
+            else
+                breadcrumbsElement.innerHTML += `<a data-link="${crumbs[key]}">${key}</a>`;
+            if (index < arr.length - 1)
+                breadcrumbsElement.innerHTML += '<i>/</i>';
+        });
+       
+    }
+    function GetCrumbsFromLink(link) {
+        if (link == null || link == '') return;
+        link = link.split('/');
+        // If the last element has a '.' (file extension)
+        if (link.slice(-1)[0].includes('.')) {
+            // Remove it form the array (.pop())
+            // Remove the '.' and everything after it
+            // Then add it back to the array (.push())
+            link.push(link.pop().split('.')[0]);
+        }
+        console.log(link)
+    }
+    */
+
 })();
