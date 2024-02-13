4cb451e31bc36f941b4876009710215d96a7ff44
chrisn02 <chrisnorman645@gmail.com>
2024-02-13 15:15:30 +0000

    Moved styling to separate CSS file
    also renamed file type from html back to php

---
+++ testing-page.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing Page</title>
+    <link rel="stylesheet" href="style.css">
+</head> 
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Stats</a></li>
+                <li><a href="#">Leaderboards</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="test-info-container">
+    <div class="test-info">
+        <p>Subject</p>
+        <p>Test Name</p>
+    </div>
+    </div>
+    <div class="question">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <div class="question-number">
+        <p>Question 1/10</p>
+    </div>
+
+    <div class="progress-bar">
+        
+    </div>
+
+    <div class="answers">
+        <div class="answer">Answer 1</div>
+        <div class="answer">Answer 2</div>
+        <div class="answer">Answer 3</div>
+        <div class="answer">Answer 4</div>
+    </div>
+    
+    
+    
+</body>
+</html>
