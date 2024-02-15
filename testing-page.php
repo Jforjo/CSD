0ac903acb483404d830e864130f31c85233b19e9
chrisn02 <chrisnorman645@gmail.com>
2024-02-15 15:09:46 +0000

    Resized the question text box
    Only done because the questions aren't likely to be as long as the placeholder text I was using, which as a result will cause blank space on the page. Will revert back if needed though. Have also added a temporary border to it so we have a visual on where exactly the question text area is

--- testing-page.php
+++ testing-page.php
@@ -24,7 +24,7 @@
     </div>
     <div class="main-content">
     <div class="question">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Placeholder question text area</p>
     </div>
 
     <div class="question-number">
