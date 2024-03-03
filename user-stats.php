c035a39385e4b71f18e021e55f11438d880c0488
chrisn02 <chrisnorman645@gmail.com>
2024-03-03 19:07:07 +0000

    Changed the label text to white and added more placeholder content for chart
    Also switched bar chart to line graph. Currently unsure which one to use though

--- user-stats.php
+++ user-stats.php
@@ -75,28 +75,50 @@
     <script>
         var ctx = document.getElementById('chart').getContext('2d');
         var chart = new Chart(ctx, {
-            type: 'bar', // specify the type of chart
+            type: 'line',
             data: {
-                labels: ['Maths Quiz 1', 'Maths Quiz 2', 'Physics Test'],
+                labels: ['Maths Quiz 1', 'Maths Quiz 2', 'Physics Test', 'Test', 'Test', 'Test', 'Test', 'Test'],
                 datasets: [{
                     label: 'Percentage',
-                    data: [100, 85, 84],
-                    backgroundColor: [
-                        'darkred',
-                        'darkred',
-                        'darkred'
+                    data: [100, 85, 84, 50, 57, 81, 73, 65],
+                    backgroundColor: 
+                    [
+                        'red'
                     ],
-                    borderColor: [
-                        'darkred',
-                        'darkred',
-                        'darkred'
+                    borderColor: 
+                    [
+                        'red'
                     ],
                     borderWidth: 1
                 }]
             },
-            options: {
-                scales: {
-                    y: {
+            options: 
+            {
+                plugins:
+                {
+                    legend:
+                    {
+                        labels:
+                        {
+                            color: 'white'
+                        }
+                    }
+                },
+                scales:
+                {
+                    x:
+                    {
+                        ticks:
+                        {
+                            color: 'white'
+                        }
+                    },
+                    y: 
+                    {
+                        ticks:
+                        {
+                            color: 'white'
+                        },
                         beginAtZero: true
                     }
                 }
